23dc412fcb81241741e798c23ef59cd79927425b
travinne <travinne.mutahi@moringaschool.com>
2025-05-30 07:38:57 +0300

    one_to_many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None 
+        Pet.all.append(self)
+
+
+        if owner:
+            self.set_owner(owner)
+    
+    def set_owner(self, owner):
+        if isinstance(owner, Owner):
+            self.owner = owner
+        else:
+            raise Exception("Owner must be an instance of Owner")
+    
     pass
 
 class Owner:
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Must add an instance of Pet")
+        pet.set_owner(self)
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
     pass
